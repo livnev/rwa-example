d56a0168d972390ed80c89a4195cabc5f3ff720c
Lev Livnev <lev@liv.nev.org.uk>
2020-11-08 09:23:12 -0100

    RwaConduit.sol: caller must have mkr

--- src/RwaConduit.sol
+++ src/RwaConduit.sol
@@ -1,20 +1,24 @@
 pragma solidity 0.5.12;
 
 interface GemLike {
-    function transfer(address,uint) external returns (bool);
+    function transfer(address,uint256) external returns (bool);
+    function balanceOf(address) external returns (uint256);
 }
 
 
 contract RwaConduit {
+    GemLike public gov;
     GemLike public dai;
     address public to;
 
-    constructor(address _dai, address _to) public {
+    constructor(address _gov, address _dai, address _to) public {
+        gov = GemLike(_gov);
         dai = GemLike(_dai);
         to = _to;
     }
 
     function push() public {
+        require(gov.balanceOf(msg.sender) > 0);
         dai.transfer(to, dai.balanceOf(address(this)));
     }
 }
