6258cbfd2384533fcbcde1672e962da8348331d9
Lev Livnev <lev@symbolic.partners>
2020-12-03 11:01:20 -0100

    RwaConduit.sol: add simple conduit back

--- src/RwaConduit.sol
+++ src/RwaConduit.sol
@@ -5,8 +5,24 @@ interface GemLike {
     function balanceOf(address) external returns (uint256);
 }
 
-
 contract RwaConduit {
+    GemLike public gov;
+    GemLike public dai;
+    address public to;
+
+    constructor(address _gov, address _dai, address _to) public {
+        gov = GemLike(_gov);
+        dai = GemLike(_dai);
+        to = _to;
+    }
+
+    function push() public {
+        require(gov.balanceOf(msg.sender) > 0);
+        dai.transfer(to, dai.balanceOf(address(this)));
+    }
+}
+
+contract RwaConduitAdvanced {
     // --- auth ---
     mapping (address => uint) public wards;
     mapping (address => uint) public can;
