7286b119739e50b83af985ec58b93ce4dd934741
Christopher Mooney <chris@dod.net>
2020-12-16 13:09:21 -0500

    shellcheck

--- deploy.sh
+++ deploy.sh
@@ -1,45 +1,44 @@
 #!/bin/sh
 
 ILK="RWA-001"
-ILK_ENCODED=$(seth --to-bytes32 "$(seth --from-ascii ${ILK})"))
+ILK_ENCODED=$(seth --to-bytes32 "$(seth --from-ascii ${ILK})")
 
 # build it
-SOLC_FLAGS="--optimize --optimize-runs=1"
-dapp --use solc:0.5.12 build
+SOLC_FLAGS="--optimize --optimize-runs=1" dapp --use solc:0.5.12 build
 
 # tokenize it
 RWA_TOKEN=$(dapp create RwaToken)
 
 # route it
-RWA_ROUTING_CONDUIT=$(dapp create RwaRoutingConduit ${MCD_GOV} ${MCD_DAI})
+RWA_ROUTING_CONDUIT=$(dapp create RwaRoutingConduit "${MCD_GOV}" "${MCD_DAI}")
 # TODO add hope() calls for trust
 # TODO add kiss() calls for trust
-seth send ${RWA_ROUTING_CONDUIT} 'rely(address)' ${MCD_PAUSE_PROXY}
-seth send ${RWA_ROUTING_CONDUIT} 'deny(address)' ${ETH_FROM}
+seth send "${RWA_ROUTING_CONDUIT}" 'rely(address)' "${MCD_PAUSE_PROXY}"
+seth send "${RWA_ROUTING_CONDUIT}" 'deny(address)' "${ETH_FROM}"
 
 # join it
-RWA_JOIN=$(dapp create AuthGemJoin ${MCD_VAT} ${ILK_ENCODED} ${RWA_TOKEN})
-seth send ${RWA_ROUTING_CONDUIT} 'rely(address)' ${MCD_PAUSE_PROXY}
-seth send ${RWA_ROUTING_CONDUIT} 'deny(address)' ${ETH_FROM}
+RWA_JOIN=$(dapp create AuthGemJoin "${MCD_VAT}" "${ILK_ENCODED}" "${RWA_TOKEN}")
+seth send "${RWA_JOIN}" 'rely(address)' "${MCD_PAUSE_PROXY}"
+seth send "${RWA_JOIN}" 'deny(address)' "${ETH_FROM}"
 
 # urn it
-RWA_URN=$(dapp create RwaUrn ${MCD_VAT} ${RWA_JOIN} ${MCD_DAI} ${RWA_ROUTING_CONDUIT})
+RWA_URN=$(dapp create RwaUrn "${MCD_VAT}" "${RWA_JOIN}" "${MCD_DAI}" "${RWA_ROUTING_CONDUIT}")
 # TODO add hope() calls for operator
-seth send ${RWA_FLIPPER} 'rely(address)' ${MCD_PAUSE_PROXY}
-seth send ${RWA_FLIPPER} 'deny(address)' ${ETH_FROM}
+seth send "${RWA_URN}" 'rely(address)' "${MCD_PAUSE_PROXY}"
+seth send "${RWA_URN}" 'deny(address)' "${ETH_FROM}"
 
 # connect it
-RWA_CONDUIT=$(dapp create RwaConduit ${MCD_GOV} ${MCD_DAI} ${RWA_URN})
+RWA_CONDUIT=$(dapp create RwaConduit "${MCD_GOV}" "${MCD_DAI}" "${RWA_URN}")
 
 # flip it
-RWA_FLIPPER=$(dapp create RwaFlipper ${MCD_VAT} ${MCD_CAT} ${ILK_ENCODED})
-seth send ${RWA_FLIPPER} 'rely(address)' ${MCD_PAUSE_PROXY}
-seth send ${RWA_FLIPPER} 'deny(address)' ${ETH_FROM}
+RWA_FLIPPER=$(dapp create RwaFlipper "${MCD_VAT}" "${MCD_CAT}" "${ILK_ENCODED}")
+seth send "${RWA_FLIPPER}" 'rely(address)' "${MCD_PAUSE_PROXY}"
+seth send "${RWA_FLIPPER}" 'deny(address)' "${ETH_FROM}"
 
 # price it
 RWA_LIQUIDATION_ORACLE=$(dapp create RwaLiquidationOracle)
-seth send ${RWA_LIQUIDATION_ORACLE} 'rely(address)' ${MCD_PAUSE_PROXY}
-seth send ${RWA_LIQUIDATION_ORACLE} 'deny(address)' ${ETH_FROM}
+seth send "${RWA_LIQUIDATION_ORACLE}" 'rely(address)' "${MCD_PAUSE_PROXY}"
+seth send "${RWA_LIQUIDATION_ORACLE}" 'deny(address)' "${ETH_FROM}"
 
 # print it
 echo "ILK: ${ILK}"
